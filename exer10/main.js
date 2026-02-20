7d03171f82952cd6ae9728d2a117cd4110065307
karcah <muhidiinabdihaaji@gmail.com>
2026-02-20 18:55:09 +0300

    exercise 10

---
+++ exer10/main.js
@@ -0,0 +1,23 @@
+
+let library = [
+
+
+{ title:"qaamuus", 
+    author:"ahmed", 
+    year:2026
+},
+
+{
+title:"dalka",
+ author:"muhidiin",
+  year:2025},
+
+]
+    
+console.log(library[0].title);
+
+console.log(library[1].author);
+
+
+
+
